3dfaf2f5b26865677e87d9350411113073867363
Ray <lokto.kwan@gmail.com>
2021-04-18 21:56:49 -0400

    add docker compose to app

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_migrate import MigrateCommand
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -23,10 +23,21 @@ def create_app():
     db.init_app(app)
 
     manager = Manager(app)
-    manager.add_command("db", MigrateCommand)
+    manager.add_command(
+        "runserver",
+        Server(
+            host=os.environ.get("HOST", "0.0.0.0"),
+            port=int(os.environ.get("PORT", 5000)),
+        )
+    )
 
     with app.app_context():
         from server.routes import account, user  # NOQA: F401
+
+        @app.route("/")
+        def hello():
+            count = get_hit_count()
+            return "Hello World! I have been seen {} times.\n".format(count)
 
         @app.route("/ping", methods=["GET"])
         def ping():
