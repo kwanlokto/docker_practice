8b4c4474b4443fcb51e4403e383cb8365e23c61c
Ray <lokto.kwan@gmail.com>
2021-05-05 20:36:54 -0400

    add login endpoint

--- backend/server/routes/user.py
+++ backend/server/routes/user.py
@@ -5,8 +5,8 @@ from server import db
 from server.models.user import User
 
 
-@app.route("/user", methods=["POST"])
-def user():
+@app.route("/signup", methods=["POST"])
+def signup():
     """
     Create a new user
     """
@@ -35,6 +35,33 @@ def user():
             isError=False,
             message="Added new user to db",
             statusCode=200,
+        ),
+        200,
+    )
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        email = request.form.get("email")
+
+    except KeyError as err:
+        msg = f"Failed to get user. ${err}"
+        return (
+            jsonify(
+                isError=True,
+                message=msg,
+                statusCode=400,
+            ),
+            400,
+        )
+    user = User.query.filter_by(email=email).one()
+    return (
+        jsonify(
+            isError=False,
+            message="Success",
+            statusCode=200,
+            data=user.as_dict(),
         ),
         200,
     )
