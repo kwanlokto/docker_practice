759eff2a0cf3723a102bf21a0e6dec78d9ede00c
Ray <lokto.kwan@gmail.com>
2025-05-08 02:40:09 -0400

    fix alembic migrations

--- backend/definitions.py
+++ backend/definitions.py
@@ -1,3 +1,28 @@
-JWT_ALG = ""
-JWT_SECRET = ""
-APP_ENV = ""
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables from if we are not running in docker
+if "RUNNING_IN_DOCKER" not in os.environ:
+    # Check if .env files are present
+    env_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+
+    if not os.path.isfile(env_file_path):
+        raise Exception("App setup error: Missing .env file.")
+
+    # Load missing env variables from development .env files
+    load_dotenv()
+
+try:
+    JWT_ALG = ""
+    JWT_SECRET = ""
+    APP_ENV = ""
+
+    POSTGRES_SERVER = os.environ["POSTGRES_SERVER"]
+    POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+    POSTGRES_DB = os.environ["POSTGRES_DB"]
+    POSTGRES_USER = os.environ["POSTGRES_USER"]
+    POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+
+except KeyError as err:
+    raise Exception(f"Missing ENV variable. {err}")
--- backend/server/routes/server.py
+++ backend/server/routes/server.py
@@ -16,12 +16,13 @@ from datetime import timedelta
 webserver = Flask(__name__)
 CORS(webserver)
 # Configure Database URI
-DATABASE_URI = "mysql+mysqlconnector://{user}:{password}@{server}/{database}".format(
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASS"],
-    server=os.environ["DB_SERVER"],
-    database=os.environ["DB_NAME"],
+DATABASE_URI = "postgresql+psycopg2://{user}:{password}@{server}/{database}".format(
+    user=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    server=os.environ["POSTGRES_SERVER"],
+    database=os.environ["POSTGRES_DB"],
 )
+
 webserver.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 
 # Initialize extensions
@@ -32,26 +33,6 @@ webserver.config["JWT_SECRET_KEY"] = "your_secret_key"  # Change this to a more 
 webserver.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)  # Set expiration time for access token
 
 JWTManager(webserver)
-
-# def validate_security_token(function_reference):
-#     """
-#     Used to handle route security
-#     """
-#     token = None
-#     if "ss_auth" in request.headers:
-#         token = request.headers["ss-auth"]
-
-#     if not token:
-#         token_issue(function_reference, "Security token missing", request.remote_addr)
-
-#     try:
-#         decode(token, JWT_SECRET, algorithms=[JWT_ALG])
-
-#         # This data should be available but it is currently not used
-#         # machine_id = data['machine_id']
-#     except Exception as err:
-#         error_message = f"Token is invalid. {str(err)}"
-#         token_issue(function_reference, error_message, request.remote_addr)
 
 
 def custom_route(rule, **options):
@@ -72,40 +53,9 @@ def custom_route(rule, **options):
             except Exception as err:
                 resp_body = jsonify(message="[Cloud API] Internal server error.")
                 status_code = 500
-                # log_message(
-                #     f"Time: {datetime.now().strftime('%H:%M:%S')}\n"
-                #     f"Function: {str(function_reference.__name__)}\n"
-                #     f"{type(err).__name__}: {str(err)}\n"
-                #     f"Message: {traceback.format_exc()}\n"
-                # )
 
             return (resp_body, status_code)
 
         return wrapper
 
     return decorator
-
-
-# def token_issue(function_reference, issue_type, request_ip):
-#     """
-#     Helper used to standardize logging and responses to token issues. This will log a message and
-#     return a response.
-
-#     Parameters:
-#         issue_type (string): Type of issue with request security token. This will be used for the
-#         logs and response.
-#         request_ip (stirng): The address of the client sending the request.
-
-#     Returns:
-#         (Tuple) Response body and status code for a flask response.
-
-#     """
-
-#     log_message(
-#         f"Time: {datetime.now().strftime('%H:%M:%S')}\n"
-#         f"Function: {str(function_reference.__name__)}\n"
-#         f"Type: {issue_type}\n"
-#         f"IP: {request_ip}\n"
-#     )
-
-#     raise SecurityException(issue_type)
