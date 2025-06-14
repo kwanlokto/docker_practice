c7284f1403b0b740ff335357f17587c454f096c7
Ray Kwan <rkwan@skatescribe.com>
2025-06-14 17:03:16 -0400

    use base helper functions

--- frontend/src/data-handler/auth.js
+++ frontend/src/data-handler/auth.js
@@ -1,73 +1,44 @@
 import axios from 'axios';
+import { axios_delete, axios_get, axios_post } from './base';
 
 const API = 'http://localhost:5000';
 
 /* Functions for User */
 export const userLogin = async (email, password) => {
-  return axios
-    .post(`${API}/user/login`, { email, password })
-    .then((res) => {
-      return res;
-    })
+  return axios_post(`${API}/user/login`, { email, password });
 };
 
 export const userSignup = async (email, password, firstName, lastName) => {
-  return axios
-    .post(`${API}/user/signup`, {
-      first_name: firstName,
-      last_name: lastName,
-      email: email,
-      password: password,
-    })
-    .then((res) => {
-      return res;
-    })
+  return axios_post(`${API}/user/signup`, {
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    password: password,
+  });
 };
 
 /* Functions for Accounts */
 export const getAllAccounts = async () => {
-  return axios
-    .get(`${API}/account`)
-    .then((res) => {
-      return res;
-    })
-
+  return axios_get(`${API}/account`);
 };
 
 export const createNewAccount = async (name) => {
-  return axios
-    .post(`${API}/account`, {
-      name: name,
-    })
-    .then((res) => {
-      return res;
-    })
+  return axios_post(`${API}/account`, {
+    name: name,
+  });
 };
 
 export const deleteAccount = async (account_id) => {
-  return axios
-    .delete(`${API}/account/${account_id}`)
-    .then((res) => {
-      return res;
-    })
+  return axios_delete(`${API}/account/${account_id}`);
 };
 
 export const createTransaction = async (account_id, operation, value) => {
-  return axios
-    .post(`${API}/account/${account_id}/transaction`, {
-      operation: operation,
-      value: value,
-    })
-    .then((res) => {
-      return res;
-    })
-}
-
+  return axios_post(`${API}/account/${account_id}/transaction`, {
+    operation: operation,
+    value: value,
+  });
+};
 
 export const getAllTransactions = async (account_id) => {
-  return axios
-    .get(`${API}/account/${account_id}/transaction`)
-    .then((res) => {
-      return res;
-    })
-}
+  return axios_get(`${API}/account/${account_id}/transaction`);
+};
